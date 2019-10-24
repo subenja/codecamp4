2daeec61f44e6461e120c7de1887bf09d7cdf2f3
EpicHigh <prasit_tongpradit@windowslive.com>
2019-10-24 17:56:21 +0700

    [♻️] Refactor code

--- activate.sh
+++ activate.sh
@@ -2,13 +2,13 @@
 
 run_tests() {
 day=""
+read -p "Which day do you want to test? Typing 1 means the test of day 1. `echo $'\n> '`" day
 while [[ ! ${day} =~ ^[1-9]{1,2}$ ]];
     do
-        echo "Please enter the number in range of 1 or 2 digits"
-        read -p "Which day do you want to test? Typing 1 means the day1 directory. `echo $'\n> '`" day
+        read -p "Please enter the number in a range of 1 or 2 digit(s). `echo $'\n> '`" day
     done
 jest src/day${day}/main.spec.js
 }
 
-echo "Welcome to Codecamp 4 Code Kata"
+echo "Welcome to Code Camp 4 Code Kata"
 run_tests
